056b697aae3ec146c0d9fa38fa2ed1f7dd02e24e
KwangHun-Song <overgive@gmail.com>
2026-03-02 21:47:51 +0900

    CommonPopup 스크립트 작성 (Popup 상속, bool 결과 반환)

---
+++ Assets/Scripts/02.Page/CommonPopup.cs
@@ -0,0 +1,40 @@
+using Base;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommonPopup : Popup
+{
+    public override string PopupName => "Popups/CommonPopup";
+
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TextMeshProUGUI contentText;
+    [SerializeField] private Button okButton;
+    [SerializeField] private Button cancelButton;
+
+    public override UniTask ShowAsync(object enterParam = null)
+    {
+        if (enterParam is CommonPopupParam param)
+        {
+            titleText.text = param.Title;
+            contentText.text = param.Content;
+            cancelButton.gameObject.SetActive(param.HasTwoButtons);
+        }
+
+        okButton.onClick.AddListener(OnOkClicked);
+        cancelButton.onClick.AddListener(OnCancelClicked);
+
+        return base.ShowAsync(enterParam);
+    }
+
+    public override void Close(object leaveParam = null)
+    {
+        okButton.onClick.RemoveListener(OnOkClicked);
+        cancelButton.onClick.RemoveListener(OnCancelClicked);
+        base.Close(leaveParam);
+    }
+
+    private void OnOkClicked() => Close(true);
+    private void OnCancelClicked() => Close(false);
+}
